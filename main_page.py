a4353cc3f8fc85d1d8eff7dbda61aceeb1ee00cf
Bibin John <91671026+ImBibinJohn@users.noreply.github.com>
2022-04-22 12:41:07 +0530

    initial spark

---
+++ main_page.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+root = Tk()
+root.geometry('500x500')
+root.title("Registration Form")
+
+
+label = Label(root, text="Registration form",width=20,font=("bold", 20))
+label.grid(padx=90,pady=53)
+
+root.mainloop()
